e0f8765868d5940d78036041a98f522e6b9e8f6e
Bjørn Nachtwey <bjoern.nachtwey@cristie.de>
2023-11-10 12:01:07 +0100

    moved table names to array for easy adapt

--- db2-selects.sh
+++ db2-selects.sh
@@ -3,6 +3,7 @@
 ##############################################################################
 # changelog
 # date     	version	remark
+# 2023-11-10	0.1.2	moved table names to array for easy adapt
 # 2023-02-20	0.1.1	added some tables + info on list 
 # 2020-08-12	0.1.0   first version put to gitlab
 # 2020-08-XX	0.0.1   initial coding using bash
@@ -23,7 +24,7 @@
 ##############################################################################
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
+#  you may not use this file except in compliance with the License
 #  You may obtain a copy of the License at
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
@@ -43,11 +44,29 @@ then
 	exit;
 fi
 
+# define tables to analyse
+tables=("ACTIVITY_LOG"				\
+		"AF_SEGMENTS"				\
+		"AF_BITFILES"				\
+		"ARCHIVE_OBJECTS" 			\
+		"AS_SEGMENTS"				\
+		"BACKUP_OBJECTS"			\
+		"BF_AGGREGATE_ATTRIBUTES"	\
+		"BF_AGGREGATED_BITFILES"	\
+		"BF_BITFILE_EXTENTS"		\
+		"BF_DEREFERENCED_CHUNKS"	\
+		"GROUP_LEADERS"				\
+		"EXPORT_OBJECTS"			\
+		"REPLICATED_OBJECTS"		\
+		"SC_OBJECT_TRACKER"			\
+		"TSMMON_STATUS"				)
+
+
 # print headline
 printf "%25s ; %15s ; %15s ; %15s ; %25s ; %10s \n" "Tabname" "object-Count" "est. time (sec)" "object-space" "space occupied by table" "Pagesize"
 
 # for loop on *all* tables
-for tab in ACTIVITY_LOG AF_SEGMENTS AF_BITFILES ARCHIVE_OBJECTS AS_SEGMENTS BACKUP_OBJECTS BF_AGGREGATE_ATTRIBUTES BF_AGGREGATED_BITFILES BF_BITFILE_EXTENTS BF_DEREFERENCED_CHUNKS GROUP_LEADERS EXPORT_OBJECTS REPLICATED_OBJECTS SC_OBJECT_TRACKER TSMMON_STATUS
+for tab in ${tables[0]}
 do
 	count=$(db2 connect to tsmdb1 2>&1>/dev/null && db2 "select count_big(*) from tsmdb1.$tab" | tail -n 4 | head -n 1 | sed -e 's/ //g' -e 's/\.$//');
 	estim=$(awk "BEGIN {print ($count / 140000)}");
