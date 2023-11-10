46db595fd2a2c961d590062c6ef9a235cdac5332
Bjørn Nachtwey <bjoern.nachtwey@cristie.de>
2023-11-10 13:12:23 +0100

    added some tables and replaces tabs with 2 spaces

--- run-stats.sh
+++ run-stats.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 # changelog
-# date     	version	remark
-# 2023-09-20	0.2.0	forking & moved to github, add some formal stuff
-# 2020-08-12	0.1.0   first version put to gitlab
-# 2020-08-XX	0.0.1   initial coding using bash
+# date        version remark
+# 2023-11-10  0.2.1   added some tables and replaces tabs with 2 spaces 
+# 2023-09-20  0.2.0   forking & moved to github, add some formal stuff
+# 2020-08-12  0.1.0   first version put to gitlab
+# 2020-08-XX  0.0.1   initial coding using bash
 #
 ##############################################################################
 #
 #  run-stats.sh
-#	
+# 
 #   runing db2 runstats for all tables
 #
 #   The Author:
@@ -35,10 +36,35 @@
 #
 ##############################################################################
 
+# define tables to do runstats
+# => remove the ones not reorganized
 # running runstats on all tables causes no problems ... but costs time
 
+tables=(                      \
+    "ACTIVITY_LOG"            \
+    "AF_SEGMENTS"             \
+    "AF_BITFILES"             \
+    "ARCHIVE_OBJECTS"         \
+    "AS_SEGMENTS"             \
+    "BACKUP_OBJECTS"          \
+    "BF_AGGREGATE_ATTRIBUTES" \
+    "BF_AGGREGATED_BITFILES"  \
+    "BF_BITFILE_EXTENTS"      \
+    "BF_DEREFERENCED_CHUNKS"  \
+    "GROUP_LEADERS"           \
+    "EXPORT_OBJECTS"          \
+    "REPLICATED_OBJECTS"      \
+    "SC_OBJECT_TRACKER"       \
+    "SD_CHUNK_LOCATIONS"      \       
+    "SD_RECON_ORDER"          \
+    "SD_REPLICATED_CHUNKS"    \
+    "TSMMON_STATUS"           )
+
+# connect to db2
 db2 connect to tsmdb1
-for tab in ACTIVITY_LOG AF_SEGMENTS AF_BITFILES ARCHIVE_OBJECTS AS_SEGMENTS BACKUP_OBJECTS BF_AGGREGATE_ATTRIBUTES BF_AGGREGATED_BITFILES GROUP_LEADERS EXPORT_OBJECTS TSMMON_STATUS
+
+# do runstats 
+for tab in ${tables[@]}
 do
- 	db2 "RUNSTATS ON TABLE tsmdb1.$tab WITH DISTRIBUTION AND SAMPLED DETAILED INDEXES ALL"
+  db2 "RUNSTATS ON TABLE tsmdb1.$tab WITH DISTRIBUTION AND SAMPLED DETAILED INDEXES ALL"
 done
